bc52f525302065a3536c0938f084501990b6fc69
alyhaidar11 <m.alyhaidarulloh@gmail.com>
2025-09-21 10:05:47 +0700

    implementasi login, register, logout, cookie

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -17,6 +18,8 @@ class Product(models.Model):
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 
 
